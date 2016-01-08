44f3e30257ca322164c9ace32d0cc676540528da
本 机 <dbjyz@beier.local>
2016-01-08 09:19:02 +0800

    Initial Commit

---
+++ blockAndDelageta/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  blockAndDelageta
+//
+//  Created by dbjyz on 16/1/8.
+//  Copyright © 2016年 dbjyz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ blockAndDelageta/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  blockAndDelageta
+//
+//  Created by dbjyz on 16/1/8.
+//  Copyright © 2016年 dbjyz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
